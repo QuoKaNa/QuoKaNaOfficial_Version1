0ac57e2461db799b5436a80d429cc60160e2d4de
quocitspkt <quocitspkt@gmail.com>
2019-11-20 15:01:32 +0700

    Bai 40

--- TeduShop.Web/Controllers/HomeController.cs
+++ TeduShop.Web/Controllers/HomeController.cs
@@ -26,5 +26,23 @@ namespace Web.Controllers
 
             return View();
         }
+
+        [ChildActionOnly]
+        public ActionResult Footer()
+        {
+            return PartialView();
+        }
+
+        [ChildActionOnly]
+        public ActionResult Header()
+        {
+            return PartialView();
+        }
+
+        [ChildActionOnly]
+        public ActionResult Category()
+        {
+            return PartialView();
+        }
     }
 }
